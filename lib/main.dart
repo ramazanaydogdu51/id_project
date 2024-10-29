e716a306a8986e5b9b326a5ee9acb26f6eae764a
ramazanaydogdu51 <51ramazanaydogdu51@gmail.com>
2024-10-29 17:23:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: AreyCard(),
+
+
+));
+
+class AreyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Deneme bir ki')
+      ),
+
+    );
+  }
+}
+
