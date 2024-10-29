9db7c1ca414211fe2552aef2497c6ba4acbac16e
ramazanaydogdu51 <51ramazanaydogdu51@gmail.com>
2024-10-29 17:37:04 +0300

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class AreyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Deneme bir ki')
+        title: Text('Githup Test')
       ),
 
     );
